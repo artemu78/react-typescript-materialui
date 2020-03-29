7f041d1d719247a7a8fb50a4f8771df65c338b5e
72c5c02a-0038-4e33-8c91-0b49cd7a855e <artemu78@yandex.ru>
2020-03-29 10:47:37 +0400

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Small example of typescript/react with material-ui interface
+
+Install it and run:
+
+```sh
+npm install
+npm start
+```
---
+++ src/components/header.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+export const Header = () => {
+  const classes = useStyles();
+  return (
+  <AppBar position="static">
+    <Toolbar />
+  </AppBar>
+  )}
